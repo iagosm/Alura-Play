1012496e6526a7400bf23898ab8bf996af69caa6
iagosm <iagosousa201486@gmail.com>
2024-02-17 21:16:59 -0300

    Finalizando projeto do Alura Play.

---
+++ src/Controller/DeleteImagePathVideoController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Alura\Mvc\Controller;
+
+use Alura\Mvc\Repository\VideoRepository;
+class DeleteImagePathVideoController implements Controller
+{
+  public function __construct(private VideoRepository $videoRepository)
+  {
+  }
+
+  public function processaRequisicao(): void {
+    $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+    if ($id === null || $id === false) {
+      header('Location: /?sucesso=0');
+      return;
+    }
+
+    $success = $this->videoRepository->removeImagePath($id);
+    if ($success === false) {
+      header('Location: /?sucesso=0');
+    } else {
+      header('Location: /?sucesso=1');
+    }
+  }
+}
---
+++ src/Controller/VideoListController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alura\Mvc\Controller;
+
+use Alura\Mvc\Repository\VideoRepository;
+
+class VideoListController implements Controller
+{
+    public function __construct(private VideoRepository $videoRepository)
+    {
+    }
+
+    public function processaRequisicao(): void
+    {
+        $videoList = $this->videoRepository->all();
+        // var_dump($videoList);exit;
+        require_once __DIR__ . '/../../views/video-list.php';
+    }
+}
